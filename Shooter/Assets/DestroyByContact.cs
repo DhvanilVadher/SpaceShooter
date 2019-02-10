4be2bd21cb7753559e8fc65cd6630fd12f33c6e6
DhvanilVadher <41858865+DhvanilVadher@users.noreply.github.com>
2019-02-10 22:59:36 +0530

    Project Uploded _[lately :)   ]
    Full project uploded

---
+++ Shooter/Assets/DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public GameObject obj2;
+	public GameObject obj3;
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+		Instantiate(obj2, transform.position, transform.rotation);
+	}
+}
---
+++ Shooter/Assets/GameController.cs
@@ -0,0 +1,75 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+	//public GameObject hazard2;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public GUIText End;
+	public GUIText Restart;
+	public GUIText RestartInHand;
+	private bool End1, Restart1;
+	private int Score;
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+		End.text = "";
+		Restart.text = "";
+		RestartInHand .text= "";
+		End1 = false;
+		Restart1 = true;
+		Score = 0;
+		//UpdateScores ();
+	}
+	void Update()
+	{
+		if(Restart1)
+		{
+			if(Input.GetKeyDown(keyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+			
+		IEnumerator SpawnWaves ()
+	{	
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Instantiate (hazard, spawnPosition, transform.rotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+			if(gameover)
+			{
+				Restart.text ="Press 'R' for Restart";
+				Restart1=true;
+				break;
+			}
+		}
+	}
+	}
+public void UpdateScore()
+{
+	score+=newScoreValue;
+	UpdareScore();
+}
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+	}
+	public void GameOver()
+	{
+		End.text="Game Over!";
+		End1=true;
+	}
+}*/
